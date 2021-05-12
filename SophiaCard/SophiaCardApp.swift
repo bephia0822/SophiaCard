707c9b77039ce8b22c180d37346fd3d598aa12b4
bephia0822 <bephia0822@gmail.com>
2021-05-12 14:07:56 +0900

    Initial Commit

---
+++ SophiaCard/SophiaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SophiaCardApp.swift
+//  SophiaCard
+//
+//  Created by Sophia Wang on 2021/5/12.
+//
+
+import SwiftUI
+
+@main
+struct SophiaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
